0654d6b7197d9e3fbcf2c000d24e051c85fbf654
Blacklist14 <luis.arigato69@hotmail.com>
2021-03-23 12:03:01 -0300

    Melhorei

--- script.js
+++ script.js
@@ -49,11 +49,11 @@ function certo(){
         if(nomex.length){
             if(nomex.toLowerCase() == pokemons[numero].nome.toLowerCase()){
                 rex.innerHTML="Acertou meu chapa!!"
-                rex.style.backgroundImage = "url(img/d575eb6b1eab9a45-.gif)"
+                rex.style.backgroundImage = "url(http://cdn.lowgif.com/full/d575eb6b1eab9a45-.gif)"
             }
             else{
                 rex.innerHTML="ERROUUUUUUUUUU!!"
-                rex.style.backgroundImage = "url(img/c97475272c3bc82d75657420a82ebb8eee9ce069_00.gif)"
+                rex.style.backgroundImage = "url(https://pa1.narvii.com/5859/c97475272c3bc82d75657420a82ebb8eee9ce069_hq.gif)"
             }
         }
 }
